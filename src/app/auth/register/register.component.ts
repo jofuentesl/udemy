6c49fac5b0c2862a27b2ca64aa9aecd7f836c2d5
jofuentesl <jofuentesle@uoc.edu>
2022-10-21 08:34:08 +0200

    reative forms

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: [ './register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  public resgisterForm = this.fb.group( { 
+    nombre: ['Jordi', [ Validators.required, Validators.minLength(3)]],
+    email: ['jordi@reprodisseny.com', [ Validators.required ]],
+    password: ['123456', [ Validators.required ]],
+    password2: ['123456', [ Validators.required ]],
+    terminos: [false, [ Validators.required ]],
+  })
+  
+  
+  constructor( private fb: FormBuilder) { }
 
-  ngOnInit(): void {
+
+  createUser() {
+    console.log( this.resgisterForm.value );
   }
 
+ 
 }
